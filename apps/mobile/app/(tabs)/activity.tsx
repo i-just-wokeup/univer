d22be48ca4c887e6dcbb3d5db160e83d4d9f4bd0
i-just-wokeup <simsim020304@gmail.com>
2026-06-22 15:43:52 +0900

    feat: 활동 탭 placeholder를 실제 ExploreScreen으로 교체

--- apps/mobile/app/(tabs)/activity.tsx
+++ apps/mobile/app/(tabs)/activity.tsx
@@ -1,13 +1,5 @@
-import { SquarePlay } from "lucide-react-native";
-
-import { TabPlaceholderScreen } from "../../src/screens/tabs/TabPlaceholderScreen";
+import { ExploreScreen } from "../../src/screens/explore/ExploreScreen";
 
 export default function ActivityRoute() {
-  return (
-    <TabPlaceholderScreen
-      description="탐색 피드와 활동성 콘텐츠를 앱 탭 구조에 맞춰 연결할 예정입니다."
-      icon={SquarePlay}
-      title="활동"
-    />
-  );
+  return <ExploreScreen />;
 }
