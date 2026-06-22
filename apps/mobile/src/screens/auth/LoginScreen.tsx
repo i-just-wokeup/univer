f51aee34a90279f0f0953d4c38b7e0cb10f9607c
i-just-wokeup <simsim020304@gmail.com>
2026-06-22 17:54:43 +0900

    style: 로그인 화면 safe area 교체

--- apps/mobile/src/screens/auth/LoginScreen.tsx
+++ apps/mobile/src/screens/auth/LoginScreen.tsx
@@ -3,12 +3,12 @@ import {
   KeyboardAvoidingView,
   Platform,
   Pressable,
-  SafeAreaView,
   StyleSheet,
   Text,
   TextInput,
   View,
 } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import { getSupabaseMobileClient } from "../../lib/supabase";
 import { colors } from "../../lib/theme";
