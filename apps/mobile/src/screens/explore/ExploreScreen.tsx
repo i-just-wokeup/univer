97238f26a1bea37d542551ee0ec5aa86dc76194a
i-just-wokeup <simsim020304@gmail.com>
2026-06-22 17:53:58 +0900

    style: 탐색 화면 safe area 교체

--- apps/mobile/src/screens/explore/ExploreScreen.tsx
+++ apps/mobile/src/screens/explore/ExploreScreen.tsx
@@ -5,13 +5,13 @@ import {
   NativeScrollEvent,
   NativeSyntheticEvent,
   RefreshControl,
-  SafeAreaView,
   ScrollView,
   StyleSheet,
   Text,
   useWindowDimensions,
   View,
 } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import { StateView } from "../../components/common/StateView";
 import { getExplorePosts } from "../../features/explore/api";
