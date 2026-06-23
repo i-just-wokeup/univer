d47e1ccde443b5e8724f9a4b7737dfe7f6d97d57
i-just-wokeup <simsim020304@gmail.com>
2026-06-23 12:52:42 +0900

    refactor: 작성 탭 라우트를 /write 리다이렉트로 변경

--- apps/mobile/app/(tabs)/write.tsx
+++ apps/mobile/app/(tabs)/write.tsx
@@ -1,5 +1,6 @@
-import { WriteScreen } from "../../src/screens/write/WriteScreen";
+import { Redirect } from "expo-router";
 
-export default function WriteRoute() {
-  return <WriteScreen />;
+// 작성은 탭 바깥 /write 라우트에서 처리한다. 이 탭으로 직접 진입하면 그쪽으로 보낸다.
+export default function WriteTabRoute() {
+  return <Redirect href="/write" />;
 }
