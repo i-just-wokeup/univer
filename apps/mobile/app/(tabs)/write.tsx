c79d66fbbd24536e9859b42cb6635166566017ed
i-just-wokeup <simsim020304@gmail.com>
2026-06-23 12:09:45 +0900

    feat: 앱 작성 탭 화면 연결

--- apps/mobile/app/(tabs)/write.tsx
+++ apps/mobile/app/(tabs)/write.tsx
@@ -1,13 +1,5 @@
-import { SquarePen } from "lucide-react-native";
-
-import { TabPlaceholderScreen } from "../../src/screens/tabs/TabPlaceholderScreen";
+import { WriteScreen } from "../../src/screens/write/WriteScreen";
 
 export default function WriteRoute() {
-  return (
-    <TabPlaceholderScreen
-      description="사진 선택, 비율 선택, 공개 범위 설정을 앱 작성 흐름으로 연결할 예정입니다."
-      icon={SquarePen}
-      title="작성"
-    />
-  );
+  return <WriteScreen />;
 }
